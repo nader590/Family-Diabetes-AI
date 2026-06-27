61cc579c9e45871de23be6bd96d734794abfbd9a
nader590 <nader.eg82020@gmail.com>
2026-06-27 11:46:19 +0300

    Connect Supabase database

--- supabase_db.py
+++ supabase_db.py
@@ -1,5 +1,14 @@
 import streamlit as st
+from supabase import create_client
 
-st.write("URL:", st.secrets["SUPABASE_URL"])
-st.write("KEY LENGTH:", len(st.secrets["SUPABASE_KEY"]))
-st.stop()
+st.write("URL =", st.secrets["SUPABASE_URL"])
+st.write("KEY =", st.secrets["SUPABASE_KEY"][:20])
+
+try:
+    supabase = create_client(
+        st.secrets["SUPABASE_URL"],
+        st.secrets["SUPABASE_KEY"]
+    )
+    st.success("Connected")
+except Exception as e:
+    st.error(str(e))
